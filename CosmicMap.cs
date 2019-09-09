ff9ddca9ed3b9713884882d4e8f4c1ba9d29d6b6
scharlegill <55111969+scharlegill@users.noreply.github.com>
2019-09-09 23:16:30 +0200

    CosmicMap
    Map api that organizes scanned units and returns information about all cosmic units in a given rectangle.

---
+++ CosmicMap.cs
@@ -0,0 +1,40 @@
+﻿using Flattiverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Map
+{
+    public class CosmicMap
+    {
+        private Vector movement = null;
+        private Vector shipmentMovement = new Vector();
+
+        private Dictionary<string, CosmicUnit> namedUnits;
+        private List<CosmicShip> ownShipUnits = new List<CosmicShip>();
+        private List<CosmicUnit> shipUnits = new List<CosmicUnit>();
+        private List<CosmicUnit> shotUnits = new List<CosmicUnit>();
+        private List<CosmicUnit> stillUnits = new List<CosmicUnit>();
+        private List<CosmicUnit> mobileUnits = new List<CosmicUnit>();
+
+        private object sync = new object();
+
+        public CosmicMap(List<Unit> scannedUnitsp)
+        {
+
+        }
+
+        public CosmicMap MergeAll(CosmicMap map)
+        {
+            return map;
+        }
+
+        public List<CosmicUnit> Query(float left, float top, float right, float bottom)
+        {
+            List<CosmicUnit> units = new List<CosmicUnit>();
+
+            return units;
+        }
+    }
+}
---
+++ CosmicUnit.cs
@@ -0,0 +1,20 @@
+﻿using Flattiverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Map
+{
+    public class CosmicUnit
+    {
+        public Vector Position;
+        public Vector MoveVector;
+        public float Radius;
+        public string Name;
+        public CosmicUnitKind Type;
+        public Team Team;
+        public bool Still;
+        public float Timeout;
+    }
+}
